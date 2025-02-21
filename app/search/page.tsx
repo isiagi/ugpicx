2495b6ed4d7186bac0b97255b59b82e502f88560
isiagi <isiagigeofrey0@gmail.com>
2025-02-21 19:27:15 +0300

    chore

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,19 +1,21 @@
-import { Header } from "@/components/header"
-import { ImageGrid } from "@/components/image-grid"
-import { Footer } from "@/components/footer"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Header } from "@/components/header";
+import { ImageGrid } from "@/components/image-grid";
+import { Footer } from "@/components/footer";
 
-export default function SearchPage({ searchParams }: { searchParams: { q: string } }) {
-  const query = searchParams.q || ""
+export default function SearchPage({ searchParams }: any) {
+  const query = searchParams.q || "";
 
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <Header showCategories={false} />
       <main className="container mx-auto px-4 py-8 flex-grow">
-        <h1 className="text-3xl font-bold mb-8">Search Results for &quot;{query}&quot;</h1>
+        <h1 className="text-3xl font-bold mb-8">
+          Search Results for &quot;{query}&quot;
+        </h1>
         <ImageGrid category={query} />
       </main>
       <Footer />
     </div>
-  )
+  );
 }
-
