63427a55173b6bf9990df1ab7e18290ccb4c958d
isiagi <isiagigeofrey0@gmail.com>
2025-02-23 18:14:57 +0300

    test

--- app/api/images/[id]/route.ts
+++ app/api/images/[id]/route.ts
@@ -30,10 +30,7 @@ export async function GET(
   }
 }
 
-export async function DELETE(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function DELETE(request: Request, { params }: { params: any }) {
   const { id } = params;
 
   try {
